d1be572eb1c33ba8fe415eb039ffaf580ae7195e
igor <igorhefner@gmail.com>
2025-07-14 12:50:11 +0200

    jwt-auth-guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { FestivalsModule } from './festivals/festivals.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-    imports:[AuthModule,PrismaModule, FestivalsModule]
+    imports:[AuthModule,PrismaModule, FestivalsModule, ReviewsModule]
 })
 export class AppModule {}
